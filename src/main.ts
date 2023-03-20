d26393e94e5f40f414ef31b355a92fdc32285eb3
Chris <christianrenner6@gmail.com>
2023-03-20 16:58:31 +0100

    try fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
 
